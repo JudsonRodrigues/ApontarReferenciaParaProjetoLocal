a675c66c4cc8a242c28d36b8a0c141f3ab8a3590
Judson de Almeida Rodrigues <judson.rodrigues@geap.com.br>
2020-03-12 14:15:07 -0300

    Console e Projeto com o servico que será acessado

---
+++ console/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using api.Service;
+
+namespace console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            WeatherForecastService a = new WeatherForecastService();
+
+            var resultado = a.Get();
+
+            foreach (var clima in resultado)
+            {
+                Console.WriteLine("Clima: " + clima.Summary
+                + " Data: " + clima.Date
+                + " Temperatura C " + clima.TemperatureC
+                + " Temperatura F " + clima.TemperatureF);
+
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
